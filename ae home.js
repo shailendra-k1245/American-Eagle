9281c3507a64d9204b60214d413358f535c4214a
Shailendra <91621557+shailendra-k1245@users.noreply.github.com>
2022-02-16 10:36:05 +0530

    rearranged the file/folder structure

---
+++ ae home.js
@@ -0,0 +1,21 @@
+// let liEls = document.querySelectorAll('ul li');
+// let index = 0;
+// window.show = function(increase) {
+//     index = index + increase;
+// index = Math.min(Math.max(index,0), liEls.length-1);
+//     liEls[index].scrollIntoView({behavior:"smooth"});
+//     }
+
+let liEls = document.querySelectorAll('ul li');
+let index = 0;
+window.show = function(increase) {
+    index = index + increase;
+    if(increase ==1) {
+        index += 4;
+    }else{
+        index -= 4;
+    }
+index = Math.min(Math.max(index,0), liEls.length-1);
+    
+    liEls[index].scrollIntoView();
+    }
